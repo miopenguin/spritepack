49ccfd82b17ab0a592c2902643bfb61af29a649d
miopenguin <mio362@gmail.com>
2020-09-07 14:22:18 +0900

    Update assets.ts

--- assets.ts
+++ assets.ts
@@ -1,29 +1,42 @@
 namespace spritePackSprites {
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile01 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile02 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile03 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile04 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile05 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile06 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile07 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile08 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile09 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile10 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile11 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile12 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile category-kenny"
     export const tile13 = image.ofBuffer(hex``);
 
 }
