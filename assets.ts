26fff6aba066260141a3a688426f357d0ccd1d61
miopenguin <mio362@gmail.com>
2020-09-07 11:05:25 +0900

    Initial files for MakeCode project

---
+++ assets.ts
@@ -0,0 +1,29 @@
+namespace spritePackSprites {
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile01 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile02 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile03 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile04 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile05 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile06 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile07 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile08 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile09 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const tile13 = image.ofBuffer(hex``);
+
+}
